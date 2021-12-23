e4cf0de0f14f1866ec3e52add8d73b3bfae19fe5
Kuzivanov Sergey <rep78398@gmail.com>
2021-12-24 02:57:35 +0300

    Task2

---
+++ Task2/Main/NullAccessor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Main
+{
+    public interface IAccessField { }
+
+    public class Index : IAccessField
+    {
+        public int Value;
+
+        public Index(int index) { Value = index; }
+    }
+
+    public class Field : IAccessField
+    {
+        public string Name;
+
+        public Field(string name) { Name = name; }
+    }
+
+    public static class NullAccessor
+    {
+        private static Expression GetField(IAccessField field, ParameterExpression resultVar, ref Type resultType, BlockExpression returnNull)
+        {
+            Expression getter;
+            if (field is Index)
+            {
+                try
+                {
+                    getter = Expression.Property(
+                        Expression.Convert(resultVar, resultType),
+                        "Item",
+                        Expression.Constant(((Index)field).Value));
+                }
+                catch
+                {
+                    getter = Expression.Constant(null);
+                }
+            }
+            else if (field is Field)
+            {
+                try
+                {
+                    getter = Expression.Field(
+                        Expression.Convert(resultVar, resultType),
+                        ((Field)field).Name);
+                }
+                catch
+                {
+                    getter = Expression.Constant(null);
+                }
+            }
+            else
+            {
+                return returnNull;
+            }
+            resultType = getter.Type;
+            var tryPart = Expression.Block(
+                typeof(void),
+                Expression.Assign(
+                    resultVar,
+                    Expression.Convert(getter, typeof(object))));
+            var catchPart = Expression.Catch(
+                typeof(Exception),
+                returnNull);
+            return Expression.TryCatch(
+                tryPart,
+                catchPart);
+        }
+
+        public static Func<T, R> Get<T, R>(params IAccessField[] fields)
+        {
+            var param = Expression.Parameter(typeof(T));
+            var resultVar = Expression.Variable(typeof(object));
+            var resultType = typeof(T);
+            var returnLabel = Expression.Label();
+            var returnNull = Expression.Block(
+                Expression.Assign(resultVar, Expression.Constant(null)),
+                Expression.Goto(returnLabel));
+            var body = new List<Expression> { Expression.Assign(resultVar, param) };
+            foreach (var field in fields)
+            {
+                body.Add(GetField(field, resultVar, ref resultType, returnNull));
+            }
+            body.Add(Expression.Label(returnLabel));
+            body.Add(Expression.Convert(resultVar, typeof(R)));
+            var func = Expression.Lambda<Func<T, R>>(
+                Expression.Block(
+                    new List<ParameterExpression> { resultVar },
+                    body),
+                param);
+            return func.Compile();
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main() { }
+    }
+}
---
+++ Task2/Tests/Tests.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Main;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private static List<int> intList = new List<int> { 13, 4, 20, 34, 1, 92 };
+
+        [Test]
+        public static void IntList_Index_1()
+        {
+            Assert.IsNull(NullAccessor.Get<List<int>, int?>(new Index(-1))(intList));
+        }
+
+        [Test]
+        public static void IntList_Index0()
+        {
+            Assert.AreEqual(NullAccessor.Get<List<int>, int?>(new Index(0))(intList), 13);
+        }
+
+        [Test]
+        public static void IntList_Index2()
+        {
+            Assert.AreEqual(NullAccessor.Get<List<int>, int?>(new Index(2))(intList), 20);
+        }
+
+        [Test]
+        public static void IntList_Index5()
+        {
+            Assert.AreEqual(NullAccessor.Get<List<int>, int?>(new Index(5))(intList), 92);
+        }
+
+        [Test]
+        public static void IntList_Index6()
+        {
+            Assert.IsNull(NullAccessor.Get<List<int>, int?>(new Index(6))(intList));
+        }
+
+        [Test]
+        public static void Index_Index0()
+        {
+            Assert.IsNull(NullAccessor.Get<Index, int?>(new Index(0))(new Index(64)));
+        }
+
+        [Test]
+        public static void Index_FieldValue()
+        {
+            Assert.AreEqual(NullAccessor.Get<Index, int?>(new Field("Value"))(new Index(64)), 64);
+        }
+
+        [Test]
+        public static void Index_FieldSomeField()
+        {
+            Assert.IsNull(NullAccessor.Get<Index, int?>(new Field("SomeField"))(new Index(64)));
+        }
+
+        private class IndexPair
+        {
+            public Index FstIndex;
+            public Index SndIndex;
+
+            public IndexPair(Index fst, Index snd) { FstIndex = fst; SndIndex = snd; }
+        }
+
+        private static IndexPair indexPair = new IndexPair(new Index(17), new Index(5));
+
+        [Test]
+        public static void IndexPair_Index1()
+        {
+            Assert.IsNull(NullAccessor.Get<IndexPair, Index>(new Index(1))(indexPair));
+        }
+
+        [Test]
+        public static void IndexPair_FieldFstIndex()
+        {
+            Assert.AreEqual(NullAccessor.Get<IndexPair, Index>(new Field("FstIndex"))(indexPair).Value, 17);
+        }
+
+        [Test]
+        public static void IndexPair_FieldSndIndex()
+        {
+            Assert.AreEqual(NullAccessor.Get<IndexPair, Index>(new Field("SndIndex"))(indexPair).Value, 5);
+        }
+
+        [Test]
+        public static void IndexPair_FieldAny()
+        {
+            Assert.IsNull(NullAccessor.Get<IndexPair, Index>(new Field("Any"))(indexPair));
+        }
+
+        [Test]
+        public static void IndexPair_FieldFstIndex_FieldValue()
+        {
+            Assert.AreEqual(NullAccessor.Get<IndexPair, int?>(new Field("FstIndex"), new Field("Value"))(indexPair), 17);
+        }
+
+        private class SomeClass
+        {
+            public List<Field> Fields;
+            public Index Index;
+
+            public SomeClass(params Field[] fields) { Fields = new List<Field>(fields); Index = new Index(Fields.Count); }
+        }
+
+        private static SomeClass someClass = new SomeClass(new Field("a"), new Field("BC"), new Field("def"));
+
+        [Test]
+        public static void SomeClass_FieldFields_Index1_FieldName()
+        {
+            Assert.AreEqual(NullAccessor.Get<SomeClass, string>(new Field("Fields"), new Index(1), new Field("Name"))(someClass), "BC");
+        }
+    }
+}
