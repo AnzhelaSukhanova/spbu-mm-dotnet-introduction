15bf03a16bddfaffde5f77bd45875360afcd0260
Kuzivanov Sergey <rep78398@gmail.com>
2021-12-24 22:27:35 +0300

    Task1

---
+++ Task1/Main/ThreadPool.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Main
+{
+    public interface IMyTask<R>
+    {
+        bool IsCompleted { get; }
+        R Result { get; }
+        IMyTask<R, NR> ContinueWith<NR>(Func<R, NR> f);
+    }
+
+    public interface IMyTask<OR, R> : IMyTask<R>
+    {
+        IMyTask<OR> BasedOn { get; }
+    }
+
+    public class SmpTask<R> : IMyTask<R>
+    {
+        private Func<R> F;
+        private R RawResult;
+        private Mutex ResultMutex;
+
+        public bool IsCompleted { get; private set; }
+
+        public SmpTask(Func<R> f)
+        {
+            F = f;
+            IsCompleted = false;
+            ResultMutex = new Mutex();
+        }
+
+        public R Result
+        {
+            get
+            {
+                if (! IsCompleted)
+                {
+                    ResultMutex.WaitOne();
+                    if (IsCompleted)
+                    {
+                        return RawResult;
+                    }
+                    try
+                    {
+                        RawResult = F();
+                    }
+                    catch (Exception e)
+                    {
+                        throw new AggregateException(e);
+                    }
+                    IsCompleted = true;
+                    ResultMutex.ReleaseMutex();
+                }
+                return RawResult;
+            }
+        }
+
+        public IMyTask<R, NR> ContinueWith<NR>(Func<R, NR> f)
+        {
+            return new CntTask<R, NR>(f, this);
+        }
+    }
+
+    public class CntTask<OR, R> : IMyTask<OR, R>
+    {
+        new private Func<OR, R> F;
+        private R RawResult;
+        private Mutex ResultMutex;
+
+        public bool IsCompleted { get; private set; }
+
+        public IMyTask<OR> BasedOn { get; private set; }
+
+        public CntTask(Func<OR, R> f, IMyTask<OR> task)
+        {
+            F = f;
+            IsCompleted = false;
+            ResultMutex = new Mutex();
+            BasedOn = task;
+        }
+        public R Result
+        {
+            get
+            {
+                if (!IsCompleted)
+                {
+                    ResultMutex.WaitOne();
+                    if (IsCompleted)
+                    {
+                        return RawResult;
+                    }
+                    try
+                    {
+                        RawResult = F(BasedOn.Result);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new AggregateException(e);
+                    }
+                    IsCompleted = true;
+                    ResultMutex.ReleaseMutex();
+                }
+                return RawResult;
+            }
+        }
+
+        public IMyTask<R, NR> ContinueWith<NR>(Func<R, NR> f)
+        {
+            return new CntTask<R, NR>(f, this);
+        }
+    }
+
+    public class ThreadPool : IDisposable
+    {
+        private List<Thread> Threads;
+        private Queue<IMyTask<object>> Tasks;
+        private Mutex MainMutex;
+        private SemaphoreSlim TasksIsEmptySemaphore;
+        private CancellationToken Cancellation;
+        private int ActiveThreadsCount;
+        private Exception GeneratedException;
+        private readonly bool IsVerbose;
+
+        private class CancellationToken : Exception { }
+
+        public int ThreadsCount { get { return Threads.Count; } }
+
+        public ThreadPool(int threadsCount, bool isVerbose = true)
+        {
+            Tasks = new Queue<IMyTask<object>>();
+            MainMutex = new Mutex();
+            TasksIsEmptySemaphore = new SemaphoreSlim(0);
+            ActiveThreadsCount = 0;
+            IsVerbose = isVerbose;
+            Threads = Enumerable.Range(0, threadsCount).Select((i) =>
+                new Thread(() => {
+                    Interlocked.Increment(ref ActiveThreadsCount);
+                    IMyTask<object> task;
+                    while (true)
+                    {
+                        MainMutex.WaitOne();
+                        if (IsVerbose) { Console.WriteLine($"Thread #{i} >>>"); }
+                        if (Tasks.Count == 0)
+                        {
+                            if (Cancellation != null)
+                            {
+                                if (IsVerbose) { Console.WriteLine($"Thread #{i} <<!"); }
+                                MainMutex.ReleaseMutex();
+                                break;
+                            }
+                            if (IsVerbose) { Console.WriteLine($"Thread #{i} <<w"); }
+                            MainMutex.ReleaseMutex();
+                            TasksIsEmptySemaphore.Wait();
+                            continue;
+                        }
+                        task = Tasks.Dequeue();
+                        if (IsVerbose) { Console.WriteLine($"Thread #{i} <<<"); }
+                        MainMutex.ReleaseMutex();
+                        try
+                        {
+                            var result = task.Result;
+                            if (IsVerbose) { Console.WriteLine($"Thread #{i} has got the result {result}"); }
+                        }
+                        catch (Exception e)
+                        {
+                            if (IsVerbose) { Console.WriteLine($"Thread #{i} !!!"); }
+                            MainMutex.WaitOne();
+                            GeneratedException = e;
+                            RawDispose();
+                            MainMutex.ReleaseMutex();
+                        }
+                    }
+                    Interlocked.Decrement(ref ActiveThreadsCount);
+                })
+            ).ToList();
+            Threads.ForEach((thread) => thread.Start());
+        }
+
+        private void ThrowGeneratedException(bool ifReleaseMutex = true)
+        {
+            if (GeneratedException != null)
+            {
+                var e = GeneratedException;
+                GeneratedException = null;
+                if (ifReleaseMutex) { MainMutex.ReleaseMutex(); }
+                throw e;
+            }
+        }
+
+        public void Enqueue(IMyTask<object> task)
+        {
+            MainMutex.WaitOne();
+            ThrowGeneratedException();
+            if (IsVerbose) { Console.WriteLine("Enqueue >>>"); }
+            if (Cancellation != null)
+            {
+                if (IsVerbose) { Console.WriteLine("Enqueue <<!"); }
+                MainMutex.ReleaseMutex();
+                return;
+            }
+            if (Tasks.Count == 0)
+            {
+                TasksIsEmptySemaphore.Release();
+            }
+            Tasks.Enqueue(task);
+            if (IsVerbose) { Console.WriteLine("Enqueue <<<"); }
+            MainMutex.ReleaseMutex();
+        }
+
+        private void RawDispose()
+        {
+            Cancellation = new CancellationToken();
+            for (int i = 0; i < ThreadsCount; i++)
+            {
+                TasksIsEmptySemaphore.Release();
+            }
+        }
+
+        public void Dispose()
+        {
+            MainMutex.WaitOne();
+            ThrowGeneratedException();
+            if (IsVerbose) { Console.WriteLine("Dispose >>>"); }
+            RawDispose();
+            if (IsVerbose) { Console.WriteLine("Dispose <<<"); }
+            MainMutex.ReleaseMutex();
+            GC.SuppressFinalize(this);
+        }
+
+        public bool IsStopped(int millisecondsTimeout)
+        {
+            var millisecondsStep = 200;
+            while (ActiveThreadsCount != 0 && millisecondsTimeout > 0)
+            {
+                Thread.Sleep(millisecondsStep);
+                millisecondsTimeout -= millisecondsStep;
+            }
+            ThrowGeneratedException(false);
+            return ActiveThreadsCount == 0;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main() { }
+    }
+}
---
+++ Task1/Tests/Tests.cs
@@ -0,0 +1,97 @@
+﻿using System.Threading;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Main;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public static void NoTask()
+        {
+            var tp = new Main.ThreadPool(3);
+            tp.Dispose();
+            Assert.IsTrue(tp.IsStopped(10000));
+        }
+
+        [Test]
+        public static void OneTask()
+        {
+            var tp = new Main.ThreadPool(3);
+            var t = new SmpTask<object>(() => {
+                Thread.Sleep(3000);
+                return null;
+            });
+            tp.Enqueue(t);
+            tp.Dispose();
+            Assert.IsTrue(tp.IsStopped(10000));
+        }
+
+        [Test]
+        public static void TaskWithException()
+        {
+            var tp = new Main.ThreadPool(3);
+            var t = new SmpTask<object>(() => {
+                Thread.Sleep(500);
+                throw new System.Exception("Something went wrong...");
+            });
+            tp.Enqueue(t);
+            tp.Dispose();
+            Assert.Throws<System.AggregateException>(() => tp.IsStopped(10000));
+        }
+
+        [Test]
+        public static void ManyTasks()
+        {
+            var tp = new Main.ThreadPool(3);
+            for (int i = 0; i < 42; i++)
+            {
+                tp.Enqueue(new SmpTask<object>(() => {
+                    Thread.Sleep(420);
+                    return i;
+                }));
+            }
+            tp.Dispose();
+            Assert.IsTrue(tp.IsStopped(10000));
+        }
+
+        [Test]
+        public static void TasksWithContinuation1()
+        {
+            var tp = new Main.ThreadPool(3);
+            var ts1 = new List<SmpTask<object>> {
+                new SmpTask<object>(() => 10),
+                new SmpTask<object>(() => { Thread.Sleep(50); return 20; }),
+                new SmpTask<object>(() => { Thread.Sleep(100); return 30; })};
+            var ts2 = new List<CntTask<object, object>>();
+            ts1.ForEach((task) => {
+                ts2.Add(new CntTask<object, object>((x) => (int)x + 1, task));
+                ts2.Add(new CntTask<object, object>((x) => { Thread.Sleep(50); return (int)x + 2; }, task));
+                ts2.Add(new CntTask<object, object>((x) => { Thread.Sleep(100); return (int)x + 3; }, task));
+            });
+            ts1.ForEach((task) => tp.Enqueue(task));
+            ts2.ForEach((task) => tp.Enqueue(task));
+            tp.Dispose();
+            Assert.IsTrue(tp.IsStopped(10000));
+        }
+
+        [Test]
+        public static void TasksWithContinuation2()
+        {
+            var tp = new Main.ThreadPool(3);
+            IMyTask<object> ts = new SmpTask<object>(() => 1);
+            tp.Enqueue(ts);
+            ts = new CntTask<object, object>((x) => { Thread.Sleep(100); return 2; }, ts);
+            tp.Enqueue(ts);
+            ts = new CntTask<object, object>((x) => { Thread.Sleep(50); return (int)x * 7; }, ts);
+            tp.Enqueue(ts);
+            ts = new CntTask<object, object>((x) => "x = " + ((int)x).ToString(), ts);
+            tp.Enqueue(ts);
+            ts = new CntTask<object, object>((x) => { Thread.Sleep(100); return ((string)x).Length; }, ts);
+            tp.Enqueue(ts);
+            tp.Dispose();
+            Assert.IsTrue(tp.IsStopped(10000));
+        }
+    }
+}
